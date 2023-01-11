3206d3ee807a879d4e60a93505205228be2ec84d
Pubudu Janith <pubudujanith123@gmail.com>
2023-01-11 07:53:35 +0530

    Set servlet mapping to path mapping

--- src/main/java/lk/ijse/dep9/api/AccountServlet.java
+++ src/main/java/lk/ijse/dep9/api/AccountServlet.java
@@ -6,7 +6,7 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "account-servlet", value = "/accounts", loadOnStartup = 0)
+@WebServlet(name = "account-servlet", value = "/accounts/*", loadOnStartup = 0)
 public class AccountServlet extends HttpServlet {
 
     @Override
--- src/main/java/lk/ijse/dep9/api/TransactionServlet.java
+++ src/main/java/lk/ijse/dep9/api/TransactionServlet.java
@@ -6,7 +6,7 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet(name = "transaction-servlet", value = "/transactions", loadOnStartup = 1)
+@WebServlet(name = "transaction-servlet", value = "/transactions/*", loadOnStartup = 1)
 public class TransactionServlet extends HttpServlet {
 
     @Override
