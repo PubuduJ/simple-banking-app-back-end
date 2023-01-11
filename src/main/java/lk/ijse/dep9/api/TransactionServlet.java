fabfa9f533a7633d4206489219c15a267a7ee6c8
Pubudu Janith <pubudujanith123@gmail.com>
2023-01-11 19:29:18 +0530

    Improve withdraw money and transfer money logics

--- src/main/java/lk/ijse/dep9/api/TransactionServlet.java
+++ src/main/java/lk/ijse/dep9/api/TransactionServlet.java
@@ -28,7 +28,7 @@ public class TransactionServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         if (request.getPathInfo() == null || request.getPathInfo().equals("/")) {
             try {
-                if (request.getContentType() == null || !request.getContentType().startsWith("application/json")){
+                if (request.getContentType() == null || !request.getContentType().startsWith("application/json")) {
                     throw new JsonException("Invalid JSON");
                 }
                 JsonParser parser = Json.createParser(request.getReader());
@@ -39,7 +39,6 @@ public class TransactionServlet extends HttpServlet {
                 if (transactionType.equalsIgnoreCase("withdraw")) {
                     TransactionDTO transactionDTO = JsonbBuilder.create().fromJson(json, TransactionDTO.class);
                     withdrawMoney(transactionDTO, response);
-
                 }
                 else if (transactionType.equalsIgnoreCase("deposit")) {
                     TransactionDTO transactionDTO = JsonbBuilder.create().fromJson(json, TransactionDTO.class);
@@ -54,6 +53,7 @@ public class TransactionServlet extends HttpServlet {
                 }
             }
             catch (JsonException e) {
+                e.printStackTrace();
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
             }
         }
@@ -155,22 +155,24 @@ public class TransactionServlet extends HttpServlet {
             /* Begin transactions */
             try {
                 connection.setAutoCommit(false);
-                PreparedStatement stmUpdate = connection.prepareStatement("UPDATE  Account SET balance = balance - ? WHERE account_number = ?");
-                stmUpdate.setBigDecimal(1, transactionDTO.getAmount());
-                stmUpdate.setString(2, transactionDTO.getAccount());
-                if (stmUpdate.executeUpdate() != 1) throw new SQLException("Failed to update the balance");
+                synchronized (this) {
+                    PreparedStatement stmUpdate = connection.prepareStatement("UPDATE  Account SET balance = balance - ? WHERE account_number = ?");
+                    stmUpdate.setBigDecimal(1, transactionDTO.getAmount());
+                    stmUpdate.setString(2, transactionDTO.getAccount());
+                    if (stmUpdate.executeUpdate() != 1) throw new SQLException("Failed to update the balance");
 
-                PreparedStatement stmNewTransaction =
-                        connection.prepareStatement("INSERT INTO Transaction (account, type, description, amount, date) VALUES (?, ?, ?, ?, ?)");
-                stmNewTransaction.setString(1, transactionDTO.getAccount());
-                stmNewTransaction.setString(2, "DEBIT");
-                stmNewTransaction.setString(3, "Withdraw");
-                stmNewTransaction.setBigDecimal(4, transactionDTO.getAmount());
-                stmNewTransaction.setTimestamp(5, new Timestamp(new Date().getTime()));
-                if (stmNewTransaction.executeUpdate() != 1) throw new SQLException("Failed to add the debit transaction record");
+                    PreparedStatement stmNewTransaction =
+                            connection.prepareStatement("INSERT INTO Transaction (account, type, description, amount, date) VALUES (?, ?, ?, ?, ?)");
+                    stmNewTransaction.setString(1, transactionDTO.getAccount());
+                    stmNewTransaction.setString(2, "DEBIT");
+                    stmNewTransaction.setString(3, "Withdraw");
+                    stmNewTransaction.setBigDecimal(4, transactionDTO.getAmount());
+                    stmNewTransaction.setTimestamp(5, new Timestamp(new Date().getTime()));
+                    if (stmNewTransaction.executeUpdate() != 1) throw new SQLException("Failed to add the debit transaction record");
 
-                Thread.sleep(5000);
-                connection.commit();
+                    Thread.sleep(5000);
+                    connection.commit();
+                }
 
                 ResultSet resultSet = stm.executeQuery();
                 resultSet.next();
@@ -226,7 +228,10 @@ public class TransactionServlet extends HttpServlet {
             PreparedStatement stm2 = connection.prepareStatement("SELECT * FROM Account WHERE account_number = ?");
             stm2.setString(1, transferDTO.getFrom());
 
-            ResultSet rst2 = stm2.executeQuery();
+            ResultSet rst2;
+            synchronized (this) {
+                rst2 = stm2.executeQuery();
+            }
             if (!rst2.next()) throw new JsonException("Invalid from account number");
 
             BigDecimal toAccountBalance = rst1.getBigDecimal("balance");
@@ -237,7 +242,7 @@ public class TransactionServlet extends HttpServlet {
 
             try {
                 connection.setAutoCommit(false);
-                PreparedStatement stmWithdraw = connection.prepareStatement("UPDATE  Account SET balance = ? WHERE account_number = ?");
+                PreparedStatement stmWithdraw = connection.prepareStatement("UPDATE Account SET balance = ? WHERE account_number = ?");
                 stmWithdraw.setBigDecimal(1, fromAccountBalance.subtract(transferDTO.getAmount()));
                 stmWithdraw.setString(2, transferDTO.getFrom());
                 if (stmWithdraw.executeUpdate() != 1) throw new SQLException("Failed to update the balance of the from account");
@@ -251,7 +256,7 @@ public class TransactionServlet extends HttpServlet {
                 stmNewTransaction.setTimestamp(5, new Timestamp(new Date().getTime()));
                 if (stmNewTransaction.executeUpdate() != 1) throw new SQLException("Failed to add the debit transaction record");
 
-                PreparedStatement stmDeposit = connection.prepareStatement("UPDATE  Account SET balance = ? WHERE account_number = ?");
+                PreparedStatement stmDeposit = connection.prepareStatement("UPDATE Account SET balance = ? WHERE account_number = ?");
                 stmDeposit.setBigDecimal(1, toAccountBalance.add(transferDTO.getAmount()));
                 stmDeposit.setString(2, transferDTO.getTo());
                 if (stmDeposit.executeUpdate() != 1) throw new SQLException("Failed to update the balance");
