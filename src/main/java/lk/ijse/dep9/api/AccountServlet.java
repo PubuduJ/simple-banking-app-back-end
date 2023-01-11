dd782906351027c1e60a24d58aa3c815f2b43431
Pubudu Janith <pubudujanith123@gmail.com>
2023-01-11 07:42:07 +0530

    Create AccountServlet and TransactionServlet

---
+++ src/main/java/lk/ijse/dep9/api/AccountServlet.java
@@ -0,0 +1,16 @@
+package lk.ijse.dep9.api;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "account-servlet", value = "/accounts", loadOnStartup = 0)
+public class AccountServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/lk/ijse/dep9/api/TransactionServlet.java
@@ -0,0 +1,16 @@
+package lk.ijse.dep9.api;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "transaction-servlet", value = "/transactions", loadOnStartup = 1)
+public class TransactionServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
