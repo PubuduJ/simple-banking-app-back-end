49b8acdd12feb22876ef2cdcf066cc689d4f982e
Pubudu Janith <pubudujanith123@gmail.com>
2023-01-11 14:39:07 +0530

    Complete data and business validation of depositMoney method

--- src/main/java/lk/ijse/dep9/api/TransactionServlet.java
+++ src/main/java/lk/ijse/dep9/api/TransactionServlet.java
@@ -13,6 +13,11 @@ import lk.ijse.dep9.dto.TransactionDTO;
 import lk.ijse.dep9.dto.TransferDTO;
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(name = "transaction-servlet", value = "/transactions/*", loadOnStartup = 1)
 public class TransactionServlet extends HttpServlet {
@@ -58,9 +63,30 @@ public class TransactionServlet extends HttpServlet {
         }
     }
 
-    private void depositMoney(TransactionDTO transactionDTO, HttpServletResponse response) {
-        System.out.println("Deposit money");
-        System.out.println(transactionDTO);
+    private void depositMoney(TransactionDTO transactionDTO, HttpServletResponse response) throws IOException {
+        try {
+            if (transactionDTO.getAccount() == null || !transactionDTO.getAccount().matches("[A-Fa-f0-9]{8}(-[A-Fa-f0-9]{4}){3}-[A-Fa-f0-9]{12}")){
+                throw new JsonException("Invalid account number");
+            }
+            else if (transactionDTO.getAmount() == null || transactionDTO.getAmount().compareTo(new BigDecimal(1000)) < 0) {
+                throw new JsonException("Invalid amount");
+            }
+            Connection connection = pool.getConnection();
+            PreparedStatement stm = connection.prepareStatement("SELECT * FROM Account WHERE account_number = ?");
+            stm.setString(1, transactionDTO.getAccount());
+            ResultSet rst = stm.executeQuery();
+            if (!rst.next()){
+                throw new JsonException("Invalid account number");
+            }
+
+        }
+        catch (JsonException e) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to deposit the money to the account");
+        }
     }
 
     private void withdrawMoney(TransactionDTO transactionDTO, HttpServletResponse response) {
